844944c8fd2678664139e84f61301dabd2b05ca9
Eric Schlossberg <eric.schlossberg@gmail.com>
2021-01-02 14:25:04 +0100

    django on heroku

--- stocks/views.py
+++ stocks/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import alpaca_trade_api as tradeapi
 
 
 # Create your views here.
 def home(request):
     return HttpResponse("Home Hi")
+
+def home2(request):
+    api = tradeapi.REST()
+    account = api.get_account()
+    api.list_positions()
