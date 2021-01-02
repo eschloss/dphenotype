cb4b962a7286e1e32b76c25923f30e22c0fd6ebc
Eric Schlossberg <eric.schlossberg@gmail.com>
2021-01-02 20:18:34 +0100

    django on heroku

--- stocks/views.py
+++ stocks/views.py
@@ -4,13 +4,18 @@ import alpaca_trade_api as tradeapi #https://pypi.org/project/alpaca-trade-api/
 import pandas as pd
 import datetime
 import pandas_datareader.data as web
+from smtplib import SMTPException
+from django.core.mail import EmailMultiAlternatives, get_connection
+from eschadmin import settings
+import logging
 
-# Create your views here.
+# Create your views here
 def home(request):
     response = ""
     api = tradeapi.REST()
-    account = api.get_account()
     end = datetime.datetime.now()
+    """
+    account = api.get_account()
     start = end - datetime.timedelta(hours=12)
     end_iso = end.isoformat()
     start_iso = start.isoformat()
@@ -18,6 +23,7 @@ def home(request):
     #upro = api.get_last_trade('UPRO')
     #api.list_positions()
     #account.status
+    """
 
     vix = web.DataReader("^VIX", "yahoo", end - datetime.timedelta(hours=24), end)
     vix_high = vix["High"]
@@ -28,6 +34,21 @@ def home(request):
     response += "^VIX Close: %s<br/>" % str(vix_close[0])
     response += "^VIX Diff: %s<br/>" % str(vix_high[0] - vix_low[0])
     response += "If ^VIX Diff is >= 9, sell<br/>"
+
+    smtp = get_connection('django.core.mail.backends.smtp.EmailBackend', host=settings.SMTP_HOST,
+                          port=settings.SMTP_PORT, username=settings.SMTP_USER, password=settings.SMTP_PASSWORD,
+                          use_tls=settings.SMTP_USE_TLS)
+    subject = "Daily ^VIX"
+    text_content = "nothing yet"
+    from_email = "info@eschadmin.heroku.com"
+    email_list = ['orpheuskl@gmail.com']
+    msg = EmailMultiAlternatives(unicode(subject), text_content, from_email, [from_email], bcc=email_list,
+                                 connection=smtp, headers=SMTP_HEADERS)
+    try:
+        msg.send(fail_silently=False)
+    except SMTPException as e:
+        logging.error("Email Failed to Send smtp=%s bcc=%s" % (bool(smtp), bcc))
+        logging.error(e, exc_info=True)
 
     #return HttpResponse(str(upro))
     return HttpResponse(response)
