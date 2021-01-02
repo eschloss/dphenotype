afc824d91c84099fe739454c5c37f4c647694620
Eric Schlossberg <eric.schlossberg@gmail.com>
2021-01-02 13:54:19 +0100

    django on heroku

--- stocks/urls.py
+++ stocks/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from eschadmin.stocks import views
+import views
 
 urlpatterns = [
     path('', views.home)
