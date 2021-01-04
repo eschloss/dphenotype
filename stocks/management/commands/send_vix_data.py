d4c36fa50d0cb9bbcc59a16b80e8ca7cd15258ae
Eric Schlossberg <eric.schlossberg@gmail.com>
2021-01-04 18:55:14 +0100

    django on heroku

--- stocks/management/commands/send_vix_data.py
+++ stocks/management/commands/send_vix_data.py
@@ -1,8 +1,14 @@
 from django.core.management.base import BaseCommand
 from stocks.tasks import send_vix_data
+from stocks.eastern_time import EST5EDT
+import datetime
 import logging
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        send_vix_data.delay()
+        now = datetime.datetime.now(tz=EST5EDT())
+        weekday = now.weekday()
+        if weekday < 5:
+            send_vix_data.delay()
+
--- stocks/management/commands/vix_near_threshold.py
+++ stocks/management/commands/vix_near_threshold.py
@@ -1,8 +1,15 @@
 from django.core.management.base import BaseCommand
 from stocks.tasks import vix_near_threshold
+from stocks.eastern_time import EST5EDT
+import datetime
 import logging
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        vix_near_threshold.delay()
+        now = datetime.datetime.now(tz=EST5EDT())
+        hour = now.hour
+        weekday = now.weekday()
+        if weekday < 5 and 4 < hour < 20:
+            vix_near_threshold.apply_async(countdown=0)
+            vix_near_threshold.apply_async(countdown=5*60)
