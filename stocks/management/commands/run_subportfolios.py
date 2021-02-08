602c060beae0a2075ee28c45e1ae70d42ceef824
Eric Schlossberg <eric.schlossberg@gmail.com>
2021-02-08 15:25:34 +0100

    fixing bugs and adding market timing in

--- stocks/management/commands/run_subportfolios.py
+++ stocks/management/commands/run_subportfolios.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from stocks.tasks import run_subportfolios
 from stocks.eastern_time import EST5EDT
 import datetime
-from rh_utils import is_market_open, trading_login
+from stocks.rh_utils import is_market_open, trading_login
 import logging
 
 
@@ -13,6 +13,6 @@ class Command(BaseCommand):
         if weekday < 5:
             trading_login()
             if is_market_open():
-                run_subportfolio.apply_async(countdown=0)
-                run_subportfolio.apply_async(countdown=5*60)
+                run_subportfolios.apply_async(countdown=0)
+                run_subportfolios.apply_async(countdown=5*60)
 
