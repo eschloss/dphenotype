d343b992f11dc49c6ef0f2c5ed2ab49b5c0d752f
Eric Schlossberg <eric.schlossberg@gmail.com>
2021-04-21 19:50:13 +0200

    adding required info to questions

--- base/views.py
+++ base/views.py
@@ -140,8 +140,10 @@ def set_question_instance(request):
                 answer = val
                 if re.search(r'^mc_', key):
                     instance = MultipleChoiceQuestionInstance.objects.get(pk=qid, profile=profile)
-                    if val == 'o' and 'mco_%s' % qid in data['answers']:
-                        instance.other_value = data['answers']['mco_%s' % qid]
+                    if val == 'o':
+                        val = -1
+                        if 'mco_%s' % qid in data['answers']:
+                            instance.other_value = data['answers']['mco_%s' % qid]
                 elif re.search(r'^ft_', key):
                     instance = FreeTextQuestionInstance.objects.get(pk=qid, profile=profile)
                 elif re.search(r'^n_', key):
