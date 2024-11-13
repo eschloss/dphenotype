53b146a0aa148fea01861862782eab80129a33d0
eschloss <eric.schlossberg@gmail.com>
2024-11-13 16:54:56 -0500

    name for string

--- base/models.py
+++ base/models.py
@@ -65,6 +65,9 @@ class Project(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 class Profile(models.Model):
     user_id = models.CharField(max_length=100, unique=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True)
